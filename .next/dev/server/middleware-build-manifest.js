e92f78901d540e2e69b9bf18a0acc069990fabe0
DhruvTambekar24 <d.tambekar24@gmail.com>
2025-12-08 12:19:45 +0530

    Modified LandingPage

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -1,31 +1,6 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": [
-      "static/chunks/5ce62_next_dist_compiled_bbc8792f._.js",
-      "static/chunks/5ce62_next_dist_shared_lib_e5585c2e._.js",
-      "static/chunks/5ce62_next_dist_client_085cc86c._.js",
-      "static/chunks/5ce62_next_dist_7cd245cc._.js",
-      "static/chunks/5ce62_next_app_daac6b6f.js",
-      "static/chunks/[next]_entry_page-loader_ts_762f1885._.js",
-      "static/chunks/dad2e_react-dom_d9101088._.js",
-      "static/chunks/4629a__pnpm_ee682280._.js",
-      "static/chunks/[root-of-the-server]__c23397a5._.js",
-      "static/chunks/OneDrive_Desktop_JobAccelerateAi_pages__app_2da965e7._.js",
-      "static/chunks/turbopack-OneDrive_Desktop_JobAccelerateAi_pages__app_b69c15a4._.js"
-    ],
-    "/_error": [
-      "static/chunks/5ce62_next_dist_compiled_bbc8792f._.js",
-      "static/chunks/5ce62_next_dist_shared_lib_b6a3c6b8._.js",
-      "static/chunks/5ce62_next_dist_client_085cc86c._.js",
-      "static/chunks/5ce62_next_dist_f18347e4._.js",
-      "static/chunks/5ce62_next_error_c6dcee7a.js",
-      "static/chunks/[next]_entry_page-loader_ts_5f60ffe5._.js",
-      "static/chunks/dad2e_react-dom_d9101088._.js",
-      "static/chunks/4629a__pnpm_ee682280._.js",
-      "static/chunks/[root-of-the-server]__5f29bb75._.js",
-      "static/chunks/OneDrive_Desktop_JobAccelerateAi_pages__error_2da965e7._.js",
-      "static/chunks/turbopack-OneDrive_Desktop_JobAccelerateAi_pages__error_cf37dafd._.js"
-    ]
+    "/_app": []
   },
   "devFiles": [],
   "polyfillFiles": [
--- .next/dev/static/chunks/OneDrive_Desktop_JobAccelerateAi_app_page_tsx_da4bb29a._.js
+++ .next/dev/static/chunks/OneDrive_Desktop_JobAccelerateAi_app_page_tsx_da4bb29a._.js
@@ -1,8 +1,13 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/OneDrive_Desktop_JobAccelerateAi_ba7877de._.js",
-  "static/chunks/4629a__pnpm_f20c44b6._.js"
+  "static/chunks/OneDrive_Desktop_JobAccelerateAi_9043b636._.js",
+  "static/chunks/5631c_tailwind-merge_dist_bundle-mjs_mjs_1c3f8960._.js",
+  "static/chunks/fe604_three_build_three_module_096937c8.js",
+  "static/chunks/74783_postprocessing_build_index_b3a5f960.js",
+  "static/chunks/12763_@tensorflow_tfjs-core_dist_tf-core_esm_9130604e.js",
+  "static/chunks/27eda_face-api_js_build_es6_d71a8b71._.js",
+  "static/chunks/4629a__pnpm_e2224607._.js"
 ],
     source: "dynamic"
 });
